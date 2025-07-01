e93be9e3040b9c73ca1a4bb7372b4fa615b2e6a5
Hitesh Goel <hhel7998@cbd.ae>
2025-07-01 09:24:39 +0400

    Added Namespace and handled for flutter 3.29+

--- android/src/main/java/hr/prijavituriste/in_app_comments/InAppCommentsPlugin.java
+++ android/src/main/java/hr/prijavituriste/in_app_comments/InAppCommentsPlugin.java
@@ -13,7 +13,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** InAppCommentsPlugin */
 public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
